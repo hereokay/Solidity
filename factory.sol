beab060a57c71925e3dd2a549cd45abae9124d7f
Chris <hereokay88@gmail.com>
2023-06-17 21:26:08 +0900

    펙토리안에 프로덕트를 관리하기 위한 numberToProduct, productTonumber 추가

--- factory.sol
+++ factory.sol
@@ -7,12 +7,18 @@ contract Product {
         number = _number;
     }
 }
-    
+
 contract Factory {
+
+    mapping(uint => address) numberToProduct; // 같은 프로덕트가 생성되지 않도록
+    mapping(address => uint) productToNumber; // 프로덕트의 숫자를 알기
     
     function createProduct(uint number) public returns(address) {
         Product created = new Product(number);
+
+        numberToProduct[number] = address(created);
+        productToNumber[address(created)] = number;
+
         return address(created);
     }
-
 }
